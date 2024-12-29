e5d35d173ed77560049d2e78af1f4478ece2e3cd
pierre8181 <prabuddhaatul@gmail.com>
2024-12-28 21:21:39 -0800

    change to 1 input

--- Autopilot_V2/Train_pilot_V2_pytorch.py
+++ Autopilot_V2/Train_pilot_V2_pytorch.py
@@ -24,7 +24,7 @@ class MyModel(nn.Module):
         # This structure must match your desired design
         self.net = nn.Sequential(
             Normalize(),
-            nn.Conv2d(3, 24, kernel_size=5, stride=2, padding=0), nn.ReLU(),
+            nn.Conv2d(1, 24, kernel_size=5, stride=2, padding=0), nn.ReLU(),
             nn.MaxPool2d(2),
             nn.Conv2d(24, 36, kernel_size=5, stride=2, padding=0), nn.ReLU(),
             nn.MaxPool2d(2),
